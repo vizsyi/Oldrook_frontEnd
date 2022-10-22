0868bc0c3973b353e5f8c870bfe6c77ef73f2cf0
Vizsy István <steve.visy@gmail.com>
2022-10-22 23:03:41 +0200

    #6: Selecting and shuffling cards

--- js/games/repplies/mcardsRepply.js
+++ js/games/repplies/mcardsRepply.js
@@ -6,4 +6,35 @@ export default class MCardsRepply extends GameRepply{
 
     }
 
+    //MCARDS FUNCTIONS
+    _shuffle(list){
+        let i, r;
+
+        for (i = list.lengthn; i > 1; ){
+            r = Math.floor(Math.random() * i--);
+            if (i != r) [list [i], list [r]] = [list [r], list [i]];
+        }
+    }
+
+    _selectingCards(list, pcs){
+        let half, i, len, r, temp;
+        const selected = [];
+
+        half = Math.floor (pcs / 2);
+        len = list.length;
+        if(half > len) throw new Error("Dummy list too short!");
+
+        for (i = 0; i < half; i++){
+            r = Math.floor(Math.random() * (len - i) + i);
+            temp = list [r];
+            if (i != r) [list [i], list [r]] = [list [r], list [i]];
+
+            temp.pairId = i;
+            temp.solved = false;
+            selected.push(temp, temp);
+        }
+
+        this._shuffle(selected);
+        return selected;
+    }
 }
