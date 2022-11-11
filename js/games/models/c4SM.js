647c7b9677d5f9ab863b6f2d429719fe698061bc
Vizsy István <steve.visy@gmail.com>
2022-11-11 22:48:43 +0100

    #7: Status bits (boolean logic)

--- js/games/models/c4SM.js
+++ js/games/models/c4SM.js
@@ -3,12 +3,15 @@ export default class MCardsSM{
         this.brickSides = [];
         this.brickSides.length = 47;
 
+        this.side = 0;
+
         this.colRest = [];
         this.colRest.length = 7;
 
+        this.bits;
     }
 
-    resetColRest (){
+    _resetColRest(){
         let col, i = 0;
         this.colRest.fill(6);
 
@@ -25,9 +28,37 @@ export default class MCardsSM{
         }
     }
 
-    reset (){
+    _setBit(pos, side, bits = this.bits){
+        if (pos > 31){
+            pos -= 32;
+            side += 2;
+        }
+
+        bits[side] |= 1 << pos;
+    }
+
+    move(col, side = this.side, bits = this.bits, colRest = this.colRest){
+        this._setBit((40 - (--colRest[col] << 3)) | col, side, bits);
+    }
+
+    nextSide(){
+        this.side ^= 1;
+    }
+
+    _resetBits(){
+        this.bits = [0, 0, 0, 0];
+        this.brickSides.forEach((side, pos) => {
+            if (side !== 2) this._setBit(pos, side);
+        })
+    }
+
+    reset(){
         this.brickSides.fill(2);
-        this.resetColRest();
+        this._resetColRest();
+
+        this._resetBits();
+
+        this.side = 0;
     }
 
 }
