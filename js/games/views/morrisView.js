c09870bdfd6e2d6ac8abc697e47ee05f975b996c
Vizsy István <steve.visy@gmail.com>
2024-02-05 23:11:45 +0100

    #8: Using ring for the morris board

--- js/games/views/morrisView.js
+++ js/games/views/morrisView.js
@@ -1,5 +1,14 @@
 import GameView from "./gameView.js";
 
+class MorrisPiece {
+
+    constructor(elem, spot = 26) {
+        this._elem = elem;
+        this._spot = spot;
+    }
+
+}
+
 export default class MorrisView extends GameView {
     constructor(factory, deskE, id)
     {
@@ -9,11 +18,11 @@ export default class MorrisView extends GameView {
         //this._statusM = new C4SM();
         
         /* View status */
-        this._nodes = [];
+        this._spots = [];
+        this._pieces = [];
   
         this._initGame();
     }
-
  
     move (col, matte, isMach){
     }
@@ -32,22 +41,52 @@ export default class MorrisView extends GameView {
 
     _initGame() {
         const //frag = this._fragmentF,
-            board = this._boardE;
-        let i, elem; 
+            board = this._boardE,
+            ring = document.createElement("div");
+        let h, i, j, elem, pieceE, pclassL = ["morris_piece"], signE; 
 
         // The web elements
         for (i = 0; i < 6; i++) {
             elem = document.createElement("div");
             elem.classList.add("morris_board_web");
-            board.appendChild(elem);
+            ring.appendChild(elem);
         }
         
-        // The nodes
+        // The spots
         for (i = 0; i < 24; i++){
             elem = document.createElement("div");
-            elem.classList.add("morris_board_node");
-            elem.setAttribute("data-ind", i);
+            elem.classList.add("morris_board_spot");
+            elem.setAttribute("data-spi", i);
+            signE = document.createElement("div");
+            signE.classList.add("board_spot_sign");
+            elem.appendChild(signE);
+            ring.appendChild(elem);
+
+            this._spots.push(elem);
+        }
+
+        // The pieces
+        for (h = 0; h < 24; h += 12){
+            elem = document.createElement("div");
+            elem.classList.add("morris_board_spot");
+            elem.setAttribute("data-spi", 28);
+            for (i = 0; i < 9; i++){
+                j = h + i;
+                pieceE = document.createElement("div");
+                pieceE.classList.add(...pclassL);
+                pieceE.setAttribute("data-pii", j);
+                pieceE.draggable = true;
+                elem.appendChild(pieceE);
+
+                this._pieces[j] = new MorrisPiece(pieceE);
+            }
             board.appendChild(elem);
+
+            if (h === 0){
+                ring.classList.add("morris_board_ring");
+                board.appendChild(ring);
+                pclassL.push("piece-dark");
+            }
         }
 
         //this._appendResultModal();
