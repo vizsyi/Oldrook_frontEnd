de216c672e331f89a96ac27f8a760c0687f21acb
Vizsy István <steve.visy@gmail.com>
2024-09-30 20:25:19 +0200

    #16: Quarto pieces and the side of the board

--- js/games/views/quartoView.js
+++ js/games/views/quartoView.js
@@ -11,7 +11,8 @@ export default class QuartoView extends GameView {
         
         /* View status */
         this._spots = [];
-        //this._pieces = [];
+        this._sideSpots = [];
+        this._pieces = [];
   
         this._initGame();
 
@@ -38,12 +39,19 @@ export default class QuartoView extends GameView {
     _boardClick(ev){
     }
 
+    _piecesRearrange(){
+        for (let i = 0; i < 16; i++){
+            this._sideSpots[i].appendChild(this._pieces[i]);
+        }
+    }
+
     _initGame() {
-        const //frag = this._fragmentF,
+        const
             board = this._boardE,
             ring = document.createElement("div"),
+            sideContainers = [document.createElement("div"), document.createElement("div")],
             div = document.createElement("div");
-        let i, elem; 
+        let i, elem, piece; 
 
         // The spots
         for (i = 0; i < 16; i++){
@@ -55,9 +63,33 @@ export default class QuartoView extends GameView {
             this._spots.push(elem);
         }
 
+        // Sides
+        sideContainers[0].classList.add("quarto_board_side");
+        sideContainers[1].classList.add("quarto_board_side", "quarto_board_side-right");
+
+        for (i = 0; i < 16; i++){
+            // Side spots
+            elem = document.createElement("div");
+            sideContainers[i&1].appendChild(elem);
+            this._sideSpots.push(elem);
+
+            // Pieces
+            piece = document.createElement("div");
+            piece.classList.add("quarto_piece");
+            if (i&1) piece.classList.add("piece-drill");
+            if (i&2) piece.classList.add("piece-dark");
+            if (i&4^4) piece.classList.add("piece-round");
+            if (i&8^8) piece.classList.add("piece-small");
+            this._pieces.push(piece);
+        }
+
+        this._piecesRearrange();
+
         ring.classList.add("quarto_board_ring");
         ring.appendChild(div);
+        board.appendChild(sideContainers[0]);
         board.appendChild(ring);
+        board.appendChild(sideContainers[1]);
 
         this._initDesk();
     }
