358743aa290fcf80e7caff68b37bc0ffeaa31333
Vizsy István <steve.visy@gmail.com>
2025-04-18 01:47:11 +0200

    #13: separating the landing and the active game codes

--- js/games/gameFactory.js
+++ js/games/gameFactory.js
@@ -10,21 +10,29 @@ import RLOG from "../log/rookLog.js";
 
 class GameFactory {
     constructor (){
+        // Landing part
+        this._landing_games = ["mcard", "c4", "morris"];
+        this._landingDiv = document.getElementById("gameLanding");
+
+        // Active part
         this._game;
         this._activeView;
         //this._activeRepply;
-        this._views = [];
 
-        this._labelSelect = document.getElementById("gameLabel");
+        this._labelSelect;
+        
+        this._control;
+        this.difficultyForm;
+
+        this._difficultyShow = false;
+
         this._activeDesk = document.getElementById("activeGameDesk");
         //this._fieldE = document.getElementById("gameField");
 
-        this._showDesks = document.querySelectorAll(".gamedesk:not(#activeGameDesk)");
-
-        this._control = document.getElementById("controlBoard");
-        this.difficultyForm = this._control?.querySelector("#difficultyForm");
-
-        this._difficultyShow = false;
+        // Obsolate
+        //this._views = [];
+        //this._showDesks = document.querySelectorAll(".gamedesk:not(#activeGameDesk)");
+        //let game = localStorage.getItem("rookgame");
 
         this._init();
         //this.addNewViews();
@@ -63,6 +71,9 @@ class GameFactory {
         return [view, game];
     }
 
+    _landing (rules){
+    }
+
     _addActiveView(game, desk, active, id){
         this._activeView?.dispo();
         [this._activeView, game] = this._createView(game, desk, active, id);
@@ -71,50 +82,6 @@ class GameFactory {
 
     _keyboardEventHandler(ev){
         this._activeView?.keyboardEvent(ev);
-    }
-
-    _init (){
-        let game;
-        
-        //Activedesk init
-        if (this._activeDesk){
-            const urlParams = new URLSearchParams(window.location.search);
-            game = urlParams.get('rg');//todo: must be lowercase
-            console.log(urlParams, "UrlParam:", game);
-
-            if (game){
-                game = this._addActiveView(game, this._activeDesk, true, 0);
-            }
-            else {
-                game = "BadC/NoRGame";
-            }
-            //let game = localStorage.getItem("rookgame");
-
-            if(this._labelSelect){
-                if (game &&
-                    [...this._labelSelect.options].some(op => op.value === game)){
-                    this._labelSelect.value = game;
-                }
-                else {
-                    this._labelSelect.value = "-";
-                }
-    
-                this._labelSelect.addEventListener("change", this._labelChange.bind(this));
-            }
-
-            document.addEventListener("keydown", this._keyboardEventHandler.bind(this));
-        }
-        else {
-            game = "index";
-            this.addNewViews();
-        }
-        
-        //todo: replace addNewViews with a block using createView
-
-        this._control?.addEventListener('click'
-            , ev => this._activeView?.repplyPlug?.controlClick(ev));
-
-        RLOG.webStart(game);//todo: webStart must accept game parameter
     }
 
     _labelChange (){
@@ -140,17 +107,86 @@ class GameFactory {
         }
     }
 
+    _init (){
+        let game;
+
+        // Landing page init
+        if (this._landingDiv){
+            game = "index";
+
+            fetch('./src/game_rules.json')
+            .then(response => response.json())
+            .then(data => {
+                this._landing(data);
+            })
+            .catch(error => {
+                console.error("Error loading the JSON file: ", error);
+                this._landing(null);
+            });
+        }
+        
+        // Activedesk init
+        if (this._activeDesk){
+            const urlParams = new URLSearchParams(window.location.search);
+            game = urlParams.get('rg');//todo: must be lowercase
+            console.log(urlParams, "UrlParam:", game);
+
+            this._labelSelect = document.getElementById("gameLabel");
+
+            this._control = document.getElementById("controlBoard");
+            this.difficultyForm = this._control?.querySelector("#difficultyForm");
+
+            if (game){
+                game = this._addActiveView(game.toLowerCase(), this._activeDesk, true, 0);
+            }
+            else {
+                game = "BadC/NoRGame";
+            }
+
+            if(this._labelSelect){
+                if (game &&
+                    [...this._labelSelect.options].some(op => op.value === game)){
+                    this._labelSelect.value = game;
+                }
+                else {
+                    this._labelSelect.value = "-";
+                }
+    
+                this._labelSelect.addEventListener("change", this._labelChange.bind(this));
+            }
+
+            document.addEventListener("keydown", this._keyboardEventHandler.bind(this));
+
+            this._control?.addEventListener('click'
+                , ev => this._activeView?.repplyPlug?.controlClick(ev));
+        }
+        /*
+        else {
+            game = "index";
+            this.addNewViews();
+        }*/
+        
+        if (game !== null){
+            game = "BadC/NoGameE";
+        }
+
+        //todo: replace addNewViews with a block using createView
+
+        RLOG.webStart(game);//todo: webStart must accept game parameter
+    }
+
     /**
      * 
      * @param {int} id 
      */
+    /*
     gameClick(id){
         //todo: must rework
         if (id < 0 || id >= this._views.length) throw new RangeError("Invalid desk index");
         this._activeView?.deactivate();
 
         this._activeView = this._views[id];
-        this._activeRepply = this._activeView.repplyPlug;
+        //this._activeRepply = this._activeView.repplyPlug;
         this._activeView.activate();
 
         // Class Cleaning
@@ -160,7 +196,9 @@ class GameFactory {
         // });
 
     }
+        */
     
+/*
     addNewViews (){
         let view;
         if (this._showDesks.length < 2) throw RangeError("Too less game desks");
@@ -177,6 +215,7 @@ class GameFactory {
             view = new MorrisView(this, this._showDesks[2], false, 2);
         }
     }
+*/
 
 /*  addNewView (game = "mcard"){
         if (this._game !== game){
