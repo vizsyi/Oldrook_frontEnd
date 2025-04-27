8114ebb42267b20cf8809b6e3a4902f0b6c53a24
Vizsy István <steve.visy@gmail.com>
2025-04-27 23:10:16 +0200

    #17: piece moving in tertio View

--- js/games/gameConfig.js
+++ js/games/gameConfig.js
@@ -1,2 +1,2 @@
-export const GAMEFIELD_CLASSES = ["gamefield"];
+//export const GAMEFIELD_CLASSES = ["gamefield"];
 //export const GAMEBOARD_CLASSES = ["gamedesk_board"];
--- js/games/views/tertioView.js
+++ js/games/views/tertioView.js
@@ -1,47 +1,117 @@
 import GameView from "./gameView.js";
 
 export default class TertioView extends GameView {
+    //static _clickClasses = ["clickspot", "clickpiece"];
+
     constructor(factory, deskE, active, id)
     {
-        super(factory, deskE, active, id, "Tertio", "tertio_board", 0, true
-            ,"tertio_board_spot");
+        super(factory, deskE, active, id, "Tertio", "tertio_board", 0, true);
 
-        /*Status*/
-        //this._statusM = new C4SM();
-        
         /* View status */
         this._spots = [];
         this._sideSpots = [];
         this._pieces = [];
+
+        this._selectedPcE;
+        this._lastSpotE;
+
+        this._clickState = null;
   
         this._initGame();
+    }
 
-        //keyboard event status
-        //this._keyActivated = 23;
-        //this._isKeyActivatedShown = false;
+    _noSelected(){
+        if (this._selectedPcE){
+            this._selectedPcE.classList.remove("selected");
+            this._selectedPcE = null;
+        }
+    }
+
+    _noClick(){
+        if (this._clickState){
+            this._boardE.classList.remove("click-" + this._clickState);
+            this._clickState = null;
+        }
+    }
+
+    addClick(isSelect){
+        cl = isSelect ? "piece" : "spot";
+        if(cl !== this._clickState){
+            this._noClick();
+            this._clickState = cl;
+            this._boardE.classList.add("click-" + cl);
+        }
     }
  
-    move (col, matte, isMach){
+    move (pci, spi){
+        const pieceE = this._pieces[pci],
+            spotE = this._spots[spi];
+
+        // Clearing click and selected classes
+        this._noSelected();
+        this._noClick();
+
+        // Clearing spot emptiness
+        spotE.classList.remove("gspot-empty");
+
+        // Moving the piece
+        spotE.appendChild(pieceE);
+
+        // Moving lastSpot class to the recent
+        this._lastSpotE.remove("gspot-last");
+        this._lastSpotE = spotE;
+        spotE.classList.add("gspot-last");
+
     }
 
-    _showCursor(IdSpot){
-        //todo: implement
-    }
+    select (pci){
+        const piece = _pieces[pci];
 
-    _hideCursor(IdSpot){
-        //todo: implement
-    }
+        this._noSelected();
+        this._noClick();
 
-    // Keyboard handlers
-    _coursorEvent(keyC){
+        this._selectedPcE = piece;
+        piece.classList.add("selected");
     }
 
     _boardClick(ev){
+        let elem, id;
+        if(this._clickState){
+
+            // Spot click
+            if(this._clickState = "spot"){
+
+                elem = ev.target.closest(".tertio_board_spot");
+                if (elem?.classList.contains("gspot-empty")){
+                    id = Number.parseInt(elem.getAttribute("data-spi"));
+                    // Intend
+                    this._repplyPlug?.moveIntend(id);
+                }
+            }
+
+            // Piece click
+            if(this._clickState = "piece"){
+
+                elem = ev.target.closest(".quarto_piece");
+                if (elem?.closest(".tertio_board_side") ){
+                    id = Number.parseInt(elem.getAttribute("data-pci"));
+                    // Intend
+                    this._repplyPlug?.selectIntend(id);
+                }
+            }
+        }
+
     }
 
-    _piecesRearrange(){
+    _pieceSpotRearrange(){
+        // Moving the pieces to thir original side spot
         for (let i = 0; i < 8; i++){
             this._sideSpots[i].appendChild(this._pieces[i]);
+        }
+
+        // Making the spots empty for cursor
+        for (let i = 0; i < 9; i++){
+            this._spots[i].classList.add("gspot-empty");
         }
     }
 
@@ -75,14 +145,15 @@ export default class TertioView extends GameView {
 
             // Pieces
             piece = document.createElement("div");
-            piece.classList.add("tertio_piece");
+            piece.setAttribute("data-pci", i);
+            piece.classList.add("quarto_piece");
             if (i&1) piece.classList.add("piece-drill");
             if (i&2) piece.classList.add("piece-dark");
             if (i&4^4) piece.classList.add("piece-round");
             this._pieces.push(piece);
         }
 
-        this._piecesRearrange();
+        this._pieceSpotRearrange();
 
         ring.classList.add("tertio_board_ring");
         ring.appendChild(div);
@@ -95,7 +166,7 @@ export default class TertioView extends GameView {
 
     newGame(){
         this._clearResult();
-        
+        this._pieceSpotRearrange();
     }
 
 }
