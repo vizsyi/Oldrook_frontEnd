52056b14dbf991f2412ffce3627218c621d55e90
Vizsy István <steve.visy@gmail.com>
2025-05-03 15:02:46 +0200

    #22: fine tuning of ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,35 +2,42 @@ import js from "@eslint/js";
 import globals from "globals";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+//import prettier from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
+  // Node_modules mappa kizárása
+  {
+    ignores: ["node_modules", "src/script.js", "olds"],
+  },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js, prettier },
+    //plugins: { js, prettier },
+    plugins: { js },
     extends: ["js/recommended"],
     rules: {
       "prefer-const": "warn",
-      "prettier/prettier": "warn"
-    }
+      "no-unused-vars": "warn",
+      "eqeqeq": ["warn", "always"]
+      //"prettier/prettier": "warn"
+    },
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
-      globals: globals.browser
-    }
+      globals: globals.browser,
+    },
   },
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
-    extends: ["markdown/recommended"]
+    extends: ["markdown/recommended"],
   },
   {
     files: ["**/*.css"],
     plugins: { css },
     language: "css/css",
-    extends: ["css/recommended"]
-  }
+    extends: ["css/recommended"],
+  },
 ]);
