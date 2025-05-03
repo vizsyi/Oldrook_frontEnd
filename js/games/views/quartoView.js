fa3493dcfb8c6a9fc63584a32c5b7b8ce7d41698
Vizsy István <steve.visy@gmail.com>
2025-05-03 15:48:21 +0200

    #16: improving Quarto view to the level of Tertio

--- js/games/views/quartoView.js
+++ js/games/views/quartoView.js
@@ -13,6 +13,11 @@ export default class QuartoView extends GameView {
         this._sideSpots = [];
         this._pieces = [];
 
+        this._selectedPcE;
+        this._lastSpotE;
+
+        this._clickState = null;
+
         this._initGame();
 
         //keyboard event status
@@ -20,9 +25,72 @@ export default class QuartoView extends GameView {
         //this._isKeyActivatedShown = false;
     }
 
-    move(col, matte, isMach) {
+    _noSelected() {
+        if (this._selectedPcE) {
+            this._selectedPcE.classList.remove("selected");
+            this._selectedPcE = null;
+        }
     }
 
+    _noClick() {
+        if (this._clickState) {
+            this._boardE.classList.remove("click-" + this._clickState);
+            this._clickState = null;
+        }
+    }
+
+    addClick(isSelect) {
+        const cl = isSelect ? "piece" : "spot";
+        if (cl !== this._clickState) {
+            this._noClick();
+            this._clickState = cl;
+            this._boardE.classList.add("click-" + cl);
+        }
+    }
+
+    move(pci, spi, finish, result, matte = null) {
+        const pieceE = this._pieces[pci],
+            spotE = this._spots[spi];
+
+        // Clearing click and selected classes
+        this._noSelected();
+        this._noClick();
+
+        // Clearing spot emptiness
+        spotE.classList.remove("gspot-empty");
+
+        // Moving the piece
+        spotE.appendChild(pieceE);
+
+        // Moving lastSpot class to the recent
+        this._lastSpotE?.classList.remove("gspot-last");
+        this._lastSpotE = spotE;
+        spotE.classList.add("gspot-last");
+
+        if (finish) {
+            if (result && matte) {
+                for (let i = 0; i < 16; i++) {
+                    if (matte & (1 << i)) {
+                        this._spots[i].classList.add("gspot-win");
+                    }
+                }
+            }
+
+            this.showResult(result);
+        }
+    }
+
+    select(pci) {
+        const piece = this._pieces[pci];
+
+        this._noSelected();
+        this._noClick();
+
+        this._selectedPcE = piece;
+        piece.classList.add("selected");
+    }
+
+    /*
     _showCursor(IdSpot) {
         //todo: implement
     }
@@ -34,19 +102,46 @@ export default class QuartoView extends GameView {
     // Keyboard handlers
     _coursorEvent(keyC) {
     }
+    */
 
     _boardClick(ev) {
+        let elem, id;
+        if (this._clickState) {
+            // Spot click
+            if ((this._clickState === "spot")) {
+                elem = ev.target.closest(".tertio_board_spot");
+                if (elem?.classList.contains("gspot-empty")) {
+                    id = Number.parseInt(elem.getAttribute("data-spi"));
+                    // Intend
+                    this._repplyPlug?.moveIntend(id);
+                }
+            }
+
+            // Piece click
+            if ((this._clickState === "piece")) {
+                elem = ev.target.closest(".quarto_piece");
+                if (elem?.closest(".tertio_board_side")) {
+                    id = Number.parseInt(elem.getAttribute("data-pci"));
+                    // Intend
+                    this._repplyPlug?.selectIntend(id);
+                }
+            }
+        }
     }
 
-    _piecesRearrange() {
+    _pieceSpotRearrange() {
         for (let i = 0; i < 16; i++) {
+            // Moving the pieces to their original side spot
             this._sideSpots[i].appendChild(this._pieces[i]);
+
+            // Making the spots empty for cursor and removing winning
+            this._spots[i].classList.add("gspot-empty");
+            this._spots[i].classList.remove("gspot-win");
         }
     }
 
     _initGame() {
-        const
-            board = this._boardE,
+        const board = this._boardE,
             ring = document.createElement("div"),
             sideContainers = [document.createElement("div"), document.createElement("div")],
             div = document.createElement("div");
@@ -74,6 +169,7 @@ export default class QuartoView extends GameView {
 
             // Pieces
             piece = document.createElement("div");
+            piece.setAttribute("data-pci", i);
             piece.classList.add("quarto_piece");
             if (i & 1) piece.classList.add("piece-drill");
             if (i & 2) piece.classList.add("piece-dark");
@@ -82,7 +178,7 @@ export default class QuartoView extends GameView {
             this._pieces.push(piece);
         }
 
-        this._piecesRearrange();
+        this._pieceSpotRearrange();
 
         ring.classList.add("quarto_board_ring");
         ring.appendChild(div);
@@ -95,7 +191,7 @@ export default class QuartoView extends GameView {
 
     newGame() {
         this._clearResult();
-
+        this._pieceSpotRearrange();
     }
 
 }
